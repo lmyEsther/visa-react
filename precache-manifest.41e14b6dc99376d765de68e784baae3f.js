4a1d0f6200e3e83d24c37613233c1afaef951b16
lmyesther <esthercheremo@gmail.com>
2019-05-13 08:59:39 -0500

    Updates

--- precache-manifest.41e14b6dc99376d765de68e784baae3f.js
+++ precache-manifest.41e14b6dc99376d765de68e784baae3f.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "071409314fb5cb3c85ace6023eb8d49b",
+    "revision": "1dfd2e8816bb20c7cc1ef12eb75b7299",
     "url": "/visa-react/index.html"
   },
   {
-    "revision": "0551e4423f4f0dec12f8",
+    "revision": "3ec99d4670fb471d7a36",
     "url": "/visa-react/static/css/main.d7bbbd9f.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/visa-react/static/js/2.2849c5a1.chunk.js"
   },
   {
-    "revision": "0551e4423f4f0dec12f8",
-    "url": "/visa-react/static/js/main.2d7e45ba.chunk.js"
+    "revision": "3ec99d4670fb471d7a36",
+    "url": "/visa-react/static/js/main.00532e06.chunk.js"
   },
   {
     "revision": "b7a9fc7acfb917930e84",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/visa-react/precache-manifest.00b277b1a4e66050fef4a2bcee37402f.js"
+  "/visa-react/precache-manifest.41e14b6dc99376d765de68e784baae3f.js"
 );
 
 self.addEventListener('message', (event) => {
